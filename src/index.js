561fb78fe655b742cee06fda687450ef9861d7f1
Vitalii <svitalii138@gmail.com>
2023-02-23 20:36:32 +0200

    Chakra-UI - init

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter basename="/goit-react-hw-08-phonebook">
-        <App />
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
