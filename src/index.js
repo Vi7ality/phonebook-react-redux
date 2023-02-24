ed7de2475bebad184b08a3cdf3f540e507aabd1e
Vitalii <svitalii138@gmail.com>
2023-02-24 10:19:28 +0200

    Basename changed to current repository

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <BrowserRouter basename="/phonebook-react-redux">
         <ChakraProvider>
           <App />
         </ChakraProvider>
